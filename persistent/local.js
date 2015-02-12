bcc5272057393cd56692560cd44954f71b875aeb
Tatsuya Oiwa <tatsuyaoiw@gmail.com>
2015-02-13 08:59:37 +0900

    Added persistent local file storage

---
+++ persistent/local.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * Persistent local file storage
+ */
+function LocalStorage() {}
+
+LocalStorage.prototype.setItem = function(key, value) {
+
+};
+
+LocalStorage.prototype.removeItem = function(key) {
+
+};
+
+module.exports = LocalStorage;
